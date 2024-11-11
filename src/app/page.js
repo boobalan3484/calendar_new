f4e52b0b1034e5ac98064e66b8fc3591459730d7
boobalan3484 <boobalan3484@gmail.com>
2024-11-11 11:27:14 +0530

    layout created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Main from "@/layout/Main";
+
+export const metadata = {
+  title: "Tamil Daily Calendar",
+  // description: "Creote | Business Landing Page",
+};
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <Main />
   );
 }
---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '@/component/Header'
+import Content from '@/component/Content'
+
+const Main = () => {
+    return (
+        <main>
+            <Header />
+            <Content />
+        </main>
+    )
+}
+
+export default Main

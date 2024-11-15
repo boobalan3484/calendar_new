b5cb96867e2b02db4dec25d389deb0522d741a50
Naveen20R <naveenr6385@gmail.com>
2024-11-15 09:21:33 +0530

    favicon changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,24 @@ export default function RootLayout({ children }) {
   }, []);
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta property="og:type" content="website" />
+        <title>Makkal Calender - Tamil Calender 2025</title>
+        <meta name="keywords" content="2025 tamil calendar, 2025 tamil monthly calendar" />
+        <meta name="description" content="Best website for tamil monthly calendar 2025." />
+        <meta property="og:title" content="Makkal Calender - Tamil Calender 2025" />
+        <meta property="og:description" content="Best website for tamil monthly calendar 2025." />
+        <meta name="twitter:title" content="Makkal Calender - Tamil Calender 2025" />
+        <meta name="twitter:description" content="Best website for tamil monthly calendar 2025." />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="googlebot-news" content="index,follow" />
+        <meta name="slurp" content="index,follow" />
+        <meta name="author" content="MPeoples Business Solutions" />
+        <link rel="icon" type="image/x-icon" href="/Makkal_Naalkaati_Logo.ico" />
+      </head>
       <body>
         {children}
       </body>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,6 +3,7 @@ import '@/style/Header.css'
 import { FaCalendarWeek } from "react-icons/fa6";
 import { FaCalendarDay } from "react-icons/fa6";
 import { FaCalendarDays } from "react-icons/fa6";
+import Link from 'next/link';
 const Header = () => {
     return (
         <header className='header'>
@@ -11,26 +12,26 @@ const Header = () => {
                     <img src="/images/Makkal_Naalkaati_Logo.png" alt="" />
                     {/* <h3 className='fw-bold'> Logo.</h3> */}
                 </div>
-                <div className="brand d-none d-lg-block" style={{width:'300px'}}>
+                <div className="brand d-none d-lg-block" style={{ width: '300px' }}>
                     <img src="/images/Makkal_Naalkaati_Logo1.png" alt="" />
                 </div>
                 <div className="share calandar-icons">
-                    <a href="/" className="day-icon" title='Day'>
+                    <Link href="/" className="day-icon" title='Day'>
                         {/* <i className="icon-day"></i> */}
                         <FaCalendarDay className='fs-3' />
                         <span className='ps-2 fw-bold'>இன்று</span>
-                    </a>
-                    <a href="/weekly-calendar" className="week-icon" title='Weekly'>
+                    </Link>
+                    <Link href="/weekly-calendar" className="week-icon" title='Weekly'>
                         {/* <i className="icon-weekly"></i> */}
                         <FaCalendarWeek className='fs-3' />
                         <span className='ps-2 fw-bold'>வாரம்</span>
-                    </a>
-                    <a href="/monthly-calendar" className="month-icon" title='Monthly'>
+                    </Link>
+                    <Link href="/monthly-calendar" className="month-icon" title='Monthly'>
                         {/* <i className="icon-monthly"></i> */}
                         <FaCalendarDays className='fs-3' />
 
                         <span className='ps-2 fw-bold'>மாதம்</span>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </header>
